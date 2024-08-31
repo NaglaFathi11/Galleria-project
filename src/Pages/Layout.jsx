5631e018c52332bc0c9b9aee107f501326957a95
NaglaFathi11 <naglafatehi11@gmail.com>
2024-08-31 15:16:02 +0200

    Add mainGalley in layout page

--- src/Pages/Layout.jsx
+++ src/Pages/Layout.jsx
@@ -1,10 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Header from "../Components/Header/Header";
+import MainGallery from "../Pages/MainGallery/MainGallery";
 
 export default function Layout() {
   return (
     <>
       <Header />
+      <MainGallery />
+
       <Outlet />
     </>
   );
